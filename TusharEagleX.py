9016d01cb138fafd87e15632b46d33ba0f75678d
tushar gurav <guravtushar231@gmail.com>
2024-11-17 10:55:46 +0530

    raw structure

---
+++ TusharEagleX.py
@@ -0,0 +1,26 @@
+try:
+    import os
+    from git import Repo
+    from flask import session
+    import sys
+    import subprocess
+    from colorama import Fore, Style, init
+    from time import sleep
+    from rich import print as rich_print
+    from rich.panel import Panel
+    from rich.table import Table
+    import urllib3
+    from rich.panel import Panel
+    from modules import open_redirection, lfi, sqli, xss, report
+
+    print("hello world")
+
+
+
+
+
+
+except KeyboardInterrupt:
+    print("Error")
+
+
